5e0c962073d41f18c9cd0e4d4d78c3ca7c5c8ae5
Hamm <majhamm@qq.com>
2025-04-02 14:24:37 +0800

    refactor(service): 移除 AbstractService 中的 loading 相关代码
    - 删除了 AbstractService 类中的 loading 属性和相关构造函数
    - 简化了 create 方法，移除了与 loading 相关的逻辑
    - 移除了与 loading 相关的类型导入
    
    # Conflicts:
    #	package.json

--- src/service/AbstractService.ts
+++ src/service/AbstractService.ts
@@ -1,5 +1,4 @@
 import type { ClassConstructor } from 'airpower'
-import type { Ref } from 'vue'
 import type { AbstractHttp } from '../http'
 import { AirModel } from 'airpower'
 
@@ -15,32 +14,10 @@ export abstract class AbstractService extends AirModel {
   abstract baseUrl: string
 
   /**
-   * ### `Loading`
-   * 你可以将这个传入的对象绑定到你需要 `Loading` 的 `DOM` 上
-   */
-  loading!: Ref<boolean>
-
-  /**
-   * ### 获取一个 `API` 服务实例
-   * @param loading `可选` Loading
-   */
-  constructor(loading?: Ref<boolean>) {
-    super()
-    if (loading) {
-      this.loading = loading
-    }
-  }
-
-  /**
    * ### 静态创建一个 `API` 服务实例
-   * @param loading `可选` Loading
    */
-  static create<S extends AbstractService>(this: ClassConstructor<S>, loading?: Ref<boolean>): S {
-    const service = new this()
-    if (loading) {
-      service.loading = loading
-    }
-    return service
+  static create<S extends AbstractService>(this: ClassConstructor<S>): S {
+    return new this()
   }
 
   /**
